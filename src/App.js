866a3de63ecce82dc9fc14083415380c7e9488f6
raju10 <trolltube272@gmail.com>
2021-02-24 18:18:46 +0600

    new update

--- src/App.js
+++ src/App.js
@@ -2,24 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayocs = [' joshim / ',' Faruk /',' Razak /',' Eliyes k']
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+   
+      
+      <Person name={nayocs[3] + nayocs[1]} job="he is a actor" actress="moushumi"></Person>
+      <Person name="Sakib khan //" job="he is the gratest actor" actress="opu bishas"></Person>
+      <Person name="Salman shah //" job="he is the Mega star of colochitro" actress="Shabnur"></Person>
+      <Person name= "Bapparaj //" job="Cheka khawa person" actress="Cheka"></Person>
+      
+      
       </header>
     </div>
   );
 }
+ export default App;
+ ///////////
 
-export default App;
+ function Person(props){
+   const personStyle= {
+    border : "1px solid red", margin: "10px",padding:"10px"
+   }
+   console.log(props)
+   return <div  style = {personStyle}>
+   <h1>Nayoc : {props.name}  {props.job}</h1>
+   <h2>Actress of : {props.actress} </h2>
+   </div>
+ }
